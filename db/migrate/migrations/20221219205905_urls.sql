783d52f12a355955823f934fbe129da60fc71356
Spear5030 <spear5030@gmail.com>
2022-12-19 21:00:05 +0300

    migrate?

---
+++ db/migrate/migrations/20221219205905_urls.sql
@@ -0,0 +1,18 @@
+-- +goose Up
+-- +goose StatementBegin
+SELECT 'up SQL query';
+
+CREATE TABLE IF NOT EXISTS urls
+(   short      VARCHAR      PRIMARY KEY,
+    long       VARCHAR      NOT NULL,
+    userID     VARCHAR      NOT NULL,
+    deleted    BOOLEAN      DEFAULT FALSE
+);
+CREATE UNIQUE INDEX long_idx1 ON urls (long);
+-- +goose StatementEnd
+
+-- +goose Down
+DROP TABLE urls;
+-- +goose StatementBegin
+SELECT 'down SQL query';
+-- +goose StatementEnd
