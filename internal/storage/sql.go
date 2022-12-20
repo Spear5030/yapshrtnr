4f3de13ff69039638d04f3e23e28fc3094e8054c
Spear5030 <spear5030@gmail.com>
2022-12-20 17:32:18 +0300

    ...

--- internal/storage/sql.go
+++ internal/storage/sql.go
@@ -84,7 +84,7 @@ func (pgStorage *pgStorage) Ping() error {
 }
 
 func (pgStorage *pgStorage) DeleteURLs(ctx context.Context, user string, shorts []string) {
-	time.AfterFunc(5*time.Second, func() {
+	time.AfterFunc(5*time.Millisecond, func() {
 		pgStorage.deleteWork <- true
 	})
 	chunk := urlsForDelete{user, shorts, ctx}
