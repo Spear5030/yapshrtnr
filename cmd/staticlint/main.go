da9c1a8a4b8217008e0ba704f9d2d3a418c953a4
Spear5030 <spear5030@gmail.com>
2023-02-16 22:47:41 +0300

    start with multichecker and fix

---
+++ cmd/staticlint/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/multichecker"
+	"golang.org/x/tools/go/analysis/passes/assign"
+	"golang.org/x/tools/go/analysis/passes/errorsas"
+	"golang.org/x/tools/go/analysis/passes/httpresponse"
+	"golang.org/x/tools/go/analysis/passes/loopclosure"
+	"golang.org/x/tools/go/analysis/passes/lostcancel"
+	"golang.org/x/tools/go/analysis/passes/nilfunc"
+	"golang.org/x/tools/go/analysis/passes/nilness"
+	"golang.org/x/tools/go/analysis/passes/printf"
+	"golang.org/x/tools/go/analysis/passes/shadow"
+	"golang.org/x/tools/go/analysis/passes/sigchanyzer"
+	"golang.org/x/tools/go/analysis/passes/sortslice"
+	"golang.org/x/tools/go/analysis/passes/stringintconv"
+	"golang.org/x/tools/go/analysis/passes/structtag"
+	"golang.org/x/tools/go/analysis/passes/tests"
+	"golang.org/x/tools/go/analysis/passes/unmarshal"
+	"golang.org/x/tools/go/analysis/passes/unreachable"
+	"golang.org/x/tools/go/analysis/passes/unsafeptr"
+	"golang.org/x/tools/go/analysis/passes/unusedresult"
+	"golang.org/x/tools/go/analysis/passes/unusedwrite"
+	"golang.org/x/tools/go/analysis/passes/usesgenerics"
+	"honnef.co/go/tools/quickfix"
+	"honnef.co/go/tools/simple"
+	"honnef.co/go/tools/staticcheck"
+	"honnef.co/go/tools/stylecheck"
+)
+
+func main() {
+
+	myСhecks := []*analysis.Analyzer{
+		printf.Analyzer,
+		shadow.Analyzer,
+		structtag.Analyzer,
+		assign.Analyzer,
+		errorsas.Analyzer,
+		httpresponse.Analyzer,
+		loopclosure.Analyzer,
+		lostcancel.Analyzer,
+		nilfunc.Analyzer,
+		nilness.Analyzer,
+		sigchanyzer.Analyzer,
+		sortslice.Analyzer,
+		stringintconv.Analyzer,
+		tests.Analyzer,
+		unmarshal.Analyzer,
+		unreachable.Analyzer,
+		unsafeptr.Analyzer,
+		unusedresult.Analyzer,
+		unusedwrite.Analyzer,
+		usesgenerics.Analyzer,
+	}
+
+	// SA
+	for _, v := range staticcheck.Analyzers {
+		myСhecks = append(myСhecks, v.Analyzer)
+	}
+	// Q
+	for _, v := range quickfix.Analyzers {
+		myСhecks = append(myСhecks, v.Analyzer)
+	}
+	// ST
+	for _, v := range stylecheck.Analyzers {
+		myСhecks = append(myСhecks, v.Analyzer)
+	}
+	// S
+	for _, v := range simple.Analyzers {
+		myСhecks = append(myСhecks, v.Analyzer)
+	}
+	fmt.Println(myСhecks)
+	multichecker.Main(myСhecks...)
+}
